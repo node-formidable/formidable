b77e06f57a84e90a1df82e2c9497a79d15d5b0f0
Felix Geisendörfer <felix@debuggable.com>
2010-05-06 11:28:08 +0200

    Moving towards boyer moore support

--- test/simple/test-multipart-parser.js
+++ test/simple/test-multipart-parser.js
@@ -9,96 +9,27 @@ var multipartParser = require('formidable/multipart_parser')
     , PROPERTIES =
       [ 'boundary'
       , 'state'
+      , 'flags'
+      , 'boyerMooreShift'
       , 'index'
+      , 'lookbehind'
       ];
 
   assert.properties(parser, PROPERTIES);
-  assert.ok(parser instanceof events.EventEmitter);
 })();
-
-// (function testWrite() {
-//   var parser = new MultipartParser();
-// 
-//   assert.throws(function() {
-//     parser.write(bufferMock);
-//   });
-// 
-//   (function testBasic() {
-//     parser.initWithBoundary('AaB03x');
-//     parser.write(parser.markerBuffer.slice(0, parser.markerBuffer.length-1));
-//     assert.equal(parser.markerIndex, parser.markerBuffer.length-1);
-// 
-//     parser.write(parser.markerBuffer.slice(parser.markerBuffer.length-1, parser.markerBuffer.length));
-//     assert.equal(parser.state, multipartParser.HEADER_END);
-//     assert.equal(parser.markerIndex, 0);
-// 
-//     p(parser);
-//   })();
-// })();
 
 (function testInitWithBoundary() {
-  var parser = new MultipartParser();
+  var parser = new MultipartParser()
+    , boundary = 'abc';
 
-  parser.initWithBoundary('abc');
-  assert.deepEqual(Array.prototype.slice.call(parser.boundary), [97, 98, 99]);
-  assert.equal(parser.state, multipartParser.STREAM_BEGIN);
+  parser.initWithBoundary(boundary);
+  assert.deepEqual(Array.prototype.slice.call(parser.boundary), [45, 45, 97, 98, 99]);
+  assert.equal(parser.state, multipartParser.START);
+
+  assert.equal(parser.boyerMooreShift.length, 256);
+  for (var i = 0; i < parser.boyerMooreShift.length; i++) {
+    assert.equal(parser.boyerMooreShift[i], boundary.length+2);
+  }
+
+  // assert.equal(parser.boyerMooreShift[97], );
 })();
-
-// (function testAdvanceState() {
-//   var parser = new MultipartParser()
-//     , callbacks = {setState: -3};
-// 
-//   parser.initWithBoundary('--abc');
-// 
-//   parser.setState = function(state) {
-//     callbacks.setState++;
-//     this.state = state;
-//   };
-// 
-//   parser.advanceState();
-//   assert.equal(parser.state, multipartParser.HEADER_END);
-// 
-//   parser.advanceState();
-//   assert.equal(parser.state, multipartParser.PART_END);
-// 
-//   parser.advanceState();
-//   assert.equal(parser.state, multipartParser.PART_BEGIN);
-// 
-//   assert.callbacks(callbacks);
-// })();
-
-// (function testSetState() {
-//   var parser = new MultipartParser();
-// 
-//   parser.boundary = new Buffer(6);
-//   parser.setState(multipartParser.PART_BEGIN);
-// 
-//   assert.equal(parser.state, multipartParser.PART_BEGIN);
-//   assert.equal(parser.markerIndex, 0);
-//   assert.deepEqual
-//     ( Array.prototype.slice.call(parser.markerBuffer)
-//     , Array.prototype.slice.call(parser.boundary).concat([13, 10])
-//     );
-// 
-//   parser.setState(multipartParser.PART_END);
-// 
-//   assert.equal(parser.state, multipartParser.PART_END);
-//   assert.equal(parser.markerIndex, 0);
-//   assert.deepEqual
-//     ( Array.prototype.slice.call(parser.markerBuffer)
-//     , [13, 10].concat(Array.prototype.slice.call(parser.boundary).concat([13, 10]))
-//     );
-// 
-// 
-//   parser.setState(multipartParser.HEADER_END);
-//   assert.deepEqual
-//     ( Array.prototype.slice.call(parser.markerBuffer)
-//     , [13, 10, 13, 10]
-//     );
-// 
-//   parser.setState(multipartParser.STREAM_END);
-//   assert.deepEqual
-//     ( Array.prototype.slice.call(parser.markerBuffer)
-//     , [13, 10].concat(Array.prototype.slice.call(parser.boundary).concat([45, 45, 13, 10]))
-//     );
-// })();
