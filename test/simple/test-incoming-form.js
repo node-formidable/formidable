588963dd50dde86b25fa008e7e6ce6a34dacbbd2
Felix Geisendörfer <felix@debuggable.com>
2010-05-17 06:30:05 -0400

    Initial integration of the multipart parser

--- test/simple/test-incoming-form.js
+++ test/simple/test-incoming-form.js
@@ -2,14 +2,24 @@ require('../common');
 var formidable = require('formidable')
   , events = require('events')
   , Buffer = require('buffer').Buffer
-  , fixtures = require('../fixture/multipart');
+  , fixtures = require('../fixture/multipart')
+  , MultipartParser = require('formidable/multipart_parser').MultipartParser
+  , sb = function(str, encoding) {
+      encoding = encoding || 'utf8';
+      // create a buffer from a string
+      var b = new Buffer(Buffer.byteLength(str, encoding));
+      b.write(str, encoding, 0);
+      return b;
+    };
 
 (function testConstructor() {
   var PROPERTIES =
       [ 'type'
       , 'headers'
+      , 'encoding'
       , 'bytesTotal'
       , 'bytesReceived'
+      , '_parser'
       ]
     , form = new formidable.IncomingForm();
 
@@ -57,19 +67,15 @@ var formidable = require('formidable')
 
   (function testEmitError() {
     var ERR = new Error('something bad happened')
-      , pauseCalled = 0
-      , emitCalled = 0;
-    form.pause = function() {
-      pauseCalled++;
-    };
-    form.emit = function(event, err) {
-      emitCalled++;
-      assert.equal(event, 'error');
+      , errorCalled = 0;
+
+    form._error = function(err) {
+      errorCalled++;
       assert.strictEqual(err, ERR);
     };
+
     emit.error(ERR);
-    assert.equal(pauseCalled, 1);
-    assert.equal(emitCalled, 1);
+    assert.equal(errorCalled, 1);
   })();
 
   (function testEmitData() {
@@ -114,13 +120,14 @@ var formidable = require('formidable')
   assert.callbacks(callbacks);
 })();
 
+(function testWrite() {
+  var form = new formidable.IncomingForm();
+  form.write();
+})();
+
 (function testParseContentType() {
   var HEADERS = {}
     , form = new formidable.IncomingForm();
-
-  form.headers = {};
-  form._parseContentType();
-  assert.strictEqual(form.type, null);
 
   form.headers = {'content-type': 'application/x-www-form-urlencoded'};
   form._parseContentType();
@@ -131,30 +138,51 @@ var formidable = require('formidable')
   form._parseContentType();
   assert.strictEqual(form.type, 'urlencoded');
 
-  var BOUNDARY = '---------------------------57814261102167618332366269';
+  var BOUNDARY = '---------------------------57814261102167618332366269'
+    , initMultipartCalled = 0;
   form.headers = {'content-type': 'multipart/form-data; boundary='+BOUNDARY};
-  form._parseContentType();
-  assert.strictEqual(form.type, 'multipart');
-  assert.strictEqual(form.boundary, BOUNDARY);
-
-  // no boundary!
-  form.headers = {'content-type': 'multipart/form-data'};
-  var emitCalled = false;
-  form.emit = function(event, err) {
-    assert.ok(event, 'error');
-
-    emitCalled = true; 
-    assert.equal(form.boundary, null);
-    assert.ok(err.message.match(/bad content-type header/i));
-  };
-
-  var pauseCalled = false;
-  form.pause = function() {
-    pauseCalled = true;
+  form._initMultipart = function(boundary) {
+    initMultipartCalled++;
+    assert.equal(boundary, BOUNDARY);
   };
   form._parseContentType();
-  assert.ok(emitCalled);
-  assert.ok(pauseCalled);
+  assert.equal(initMultipartCalled, 1);
+
+  (function testNoBoundary() {
+    form.headers = {'content-type': 'multipart/form-data'};
+    var errorCalled = 0;
+    form._error = function(err) {
+      errorCalled++;
+      assert.ok(err.message.match(/no multipart boundary/i));
+    };
+
+    form._parseContentType();
+    assert.ok(errorCalled);
+  })();
+
+  (function testNoContentType() {
+    form.headers = {};
+    var errorCalled = 0;
+    form._error = function(err) {
+      errorCalled++;
+      assert.ok(err.message.match(/no content-type/i));
+    };
+
+    form._parseContentType();
+    assert.ok(errorCalled);
+  })();
+
+  (function testUnknownContentType() {
+    form.headers = {'content-type': 'invalid'};
+    var errorCalled = 0;
+    form._error = function(err) {
+      errorCalled++;
+      assert.ok(err.message.match(/unknown content-type/i));
+    };
+
+    form._parseContentType();
+    assert.ok(errorCalled);
+  })();
 })();
 
 (function testParseContentLength() {
@@ -174,4 +202,150 @@ var formidable = require('formidable')
   form.headers['content-length'] = '08';
   form._parseContentLength();
   assert.strictEqual(form.bytesTotal, 8);
+})();
+
+(function testInitMultipart() {
+  var form = new formidable.IncomingForm()
+    , BOUNDARY = '123'
+    , parserMock = {}
+    , newParserCalled = 0
+    , initWithBoundaryCalled = 0;
+
+  parserMock.initWithBoundary = function(boundary) {
+    initWithBoundaryCalled++;
+    assert.equal(boundary, BOUNDARY);
+  };
+
+  form._newParser = function() {
+    newParserCalled++;
+    return parserMock;
+  };
+
+  form._initMultipart(BOUNDARY);
+  assert.equal(form.type, 'multipart');
+  assert.strictEqual(form._parser, parserMock);
+  assert.equal(newParserCalled, 1);
+  assert.equal(initWithBoundaryCalled, 1);
+
+  var parser = form._parser;
+
+  (function testRegularField() {
+    var onPartCalled = 0
+      , partDataEmitted = 0
+      , partEndEmitted = 0;
+
+    form.onPart = function(part) {
+      onPartCalled++;
+
+      assert.deepEqual
+        ( part.headers
+        , { 'content-disposition': 'form-data; name="field1"'
+          , 'foo': 'bar'
+          }
+        );
+      assert.equal(part.name, 'field1');
+
+      part.addListener('data', function(b) {
+        partDataEmitted++;
+        if (partDataEmitted == 1) {
+          assert.equal(b.toString(), 'hello');
+        } else if (partDataEmitted == 2) {
+          assert.equal(b.toString(), ' world');
+        }
+      });
+
+      part.addListener('end', function() {
+        partEndEmitted++;
+      });
+    };
+
+    parser.onPartBegin();
+    parser.onHeaderField(sb('content-disposition'), 0, 10);
+    parser.onHeaderField(sb('content-disposition'), 10, 19);
+    parser.onHeaderValue(sb('form-data; name="field1"'), 0, 14);
+    parser.onHeaderValue(sb('form-data; name="field1"'), 14, 24);
+    parser.onHeaderField(sb('foo'), 0, 3);
+    parser.onHeaderValue(sb('bar'), 0, 3);
+    parser.onPartData(sb('hello world'), 0, 5);
+    parser.onPartData(sb('hello world'), 5, 11);
+    parser.onPartEnd();
+    assert.equal(onPartCalled, 1);
+    assert.equal(partDataEmitted, 2);
+    assert.equal(partEndEmitted, 1);
+  })();
+
+  (function testFileField() {
+    var onPartCalled = 0
+      , partDataEmitted = 0
+      , partEndEmitted = 0;
+
+    form.onPart = function(part) {
+      onPartCalled++;
+
+      assert.deepEqual
+        ( part.headers
+        , { 'content-disposition': 'form-data; name="field2"; filename="file1.txt"'
+          , 'content-type': 'text/plain'
+          }
+        );
+      assert.equal(part.name, 'field2');
+      assert.equal(part.filename, 'file1.txt');
+      assert.equal(part.mime, 'text/plain');
+
+      part.addListener('data', function(b) {
+        partDataEmitted++;
+        assert.equal(b.toString(), '... contents of file1.txt ...');
+      });
+
+      part.addListener('end', function() {
+        partEndEmitted++;
+      });
+    };
+
+    parser.onPartBegin();
+    parser.onHeaderField(sb('content-disposition'), 0, 19);
+    parser.onHeaderValue(sb('form-data; name="field2"; filename="file1.txt"'), 0, 46);
+    parser.onHeaderField(sb('Content-Type'), 0, 12);
+    parser.onHeaderValue(sb('text/plain'), 0, 10);
+    parser.onPartData(sb('... contents of file1.txt ...'), 0, 29);
+    parser.onPartEnd();
+    assert.equal(onPartCalled, 1);
+    assert.equal(partDataEmitted, 1);
+    assert.equal(partEndEmitted, 1);
+  })();
+})();
+
+(function testInitUrlencoded() {
+  var form = new formidable.IncomingForm();
+  form._initUrlencoded();
+  assert.equal(form.type, 'urlencoded');
+})();
+
+(function testError() {
+  var form = new formidable.IncomingForm()
+    , ERR = new Error('bla')
+    , pauseCalled = 0
+    , emitCalled = 0;
+
+  form.pause = function() {
+    pauseCalled++;
+  };
+
+  form.emit = function(event, err) {
+    emitCalled++;
+
+    assert.equal(event, 'error');
+    assert.strictEqual(err, ERR);
+  };
+
+  form._error(ERR);
+  assert.equal(pauseCalled, 1);
+  assert.equal(emitCalled, 1);
+})();
+
+(function testNewParser() {
+  var form = new formidable.IncomingForm()
+    , parser = form._newParser();
+
+  assert.ok(parser instanceof MultipartParser);
 })();
