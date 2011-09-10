149353f2faf0fc99aa1b0cc3dee318ebc31b7d24
Felix Geisendörfer <felix@debuggable.com>
2011-09-10 10:19:07 -0700

    Unescape quotes for webkit

--- test/fixture/js/special-chars-in-filename.js
+++ test/fixture/js/special-chars-in-filename.js
@@ -7,12 +7,14 @@ function expect(filename) {
   ];
 };
 
+var webkit = " ? % * | \" < > . ? ; ' @ # $ ^ & ( ) - _ = + { } [ ] ` ~.txt";
+
 module.exports = {
-  'osx-chrome-13.http'   : expect(" ? % * | %22 < > . ? ; ' @ # $ ^ & ( ) - _ = + { } [ ] ` ~.txt"),
+  'osx-chrome-13.http'   : expect(webkit),
   'osx-firefox-3.6.http' : expect(" ? % * | \" < > . &#9731; ; ' @ # $ ^ & ( ) - _ = + { } [ ] ` ~.txt"),
-  'osx-safari-5.http'    : expect(" ? % * | %22 < > . ? ; ' @ # $ ^ & ( ) - _ = + { } [ ] ` ~.txt"),
-  'xp-chrome-12.http'    : expect(" ? % * | %22 < > . ? ; ' @ # $ ^ & ( ) - _ = + { } [ ] ` ~.txt"),
+  'osx-safari-5.http'    : expect(webkit),
+  'xp-chrome-12.http'    : expect(webkit),
   'xp-ie-7.http'         : expect(" ? % * | \" < > . &#9731; ; ' @ # $ ^ & ( ) - _ = + { } [ ] ` ~.txt"),
   'xp-ie-8.http'         : expect(" ? % * | \" < > . &#9731; ; ' @ # $ ^ & ( ) - _ = + { } [ ] ` ~.txt"),
-  'xp-safari-5.http'     : expect(" ? % * | %22 < > . ? ; ' @ # $ ^ & ( ) - _ = + { } [ ] ` ~.txt"),
+  'xp-safari-5.http'     : expect(webkit),
 };
