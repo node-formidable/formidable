9e11f080d62c11e34c63e9d662be24dae0757df1
Elzair <elzairthesorcerer@gmail.com>
2014-01-01 16:35:19 -0500

    Made upload example run unmodified by specifying default values for TEST_TMP and TEST_PORT

--- example/upload.js
+++ example/upload.js
@@ -1,7 +1,7 @@
 require('../test/common');
 var http = require('http'),
     util = require('util'),
-    formidable = require('../index'),
+    formidable = require('../index'), // Change this to require('formidable') in a real example
     server;
 
 var TEST_TMP = process.env.TMP || process.env.TMPDIR || process.env.TEMP || '/tmp' || process.cwd();
