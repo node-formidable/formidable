dca4f0b10eca6abd22a199a86eb5b689893278c5
Douglas Christopher Wilson <doug@somethingdoug.com>
2014-02-20 12:55:30 -0500

    Improve parsing boundary attribute from Content-Type
    This changes the regular expression to parse the Content-Type to
    generically look at the attributes, such that unknown ones are
    just ignored.

---
+++ test/fixture/js/content-type.js
@@ -0,0 +1,32 @@
+module.exports['charset.http'] = [
+  {
+    type: 'file',
+    name: 'file',
+    filename: 'plain.txt',
+    fixture: 'plain.txt',
+    sha1: 'b31d07bac24ac32734de88b3687dddb10e976872',
+    size: 23,
+  }
+];
+
+module.exports['charset-last.http'] = [
+  {
+    type: 'file',
+    name: 'file',
+    filename: 'plain.txt',
+    fixture: 'plain.txt',
+    sha1: 'b31d07bac24ac32734de88b3687dddb10e976872',
+    size: 23,
+  }
+];
+
+module.exports['custom.http'] = [
+  {
+    type: 'file',
+    name: 'file',
+    filename: 'plain.txt',
+    fixture: 'plain.txt',
+    sha1: 'b31d07bac24ac32734de88b3687dddb10e976872',
+    size: 23,
+  }
+];
