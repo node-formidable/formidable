7ce7963231974dbd608a53dbcdb63aeae90a1413
Douglas Christopher Wilson <doug@somethingdoug.com>
2014-06-16 16:55:45 -0400

    Add test to verify error implies unpipe when supported

---
+++ test/standalone/test-error-unpipe.js
@@ -0,0 +1,32 @@
+var multiparty = require('../../')
+  , assert = require('assert')
+  , http = require('http')
+  , net = require('net')
+  , stream = require('readable-stream');
+
+var form = new multiparty.Form();
+var req = new stream.Readable();
+var unpiped = false;
+
+req.headers = {
+  'content-type': 'multipart/form-data; boundary=foo'
+};
+req._read = function(){
+  this.push(new Buffer('--foo!'));
+};
+
+form.on('error', function(err){
+  // verification that error event implies unpipe call
+  assert.ok(err);
+  assert.ok(unpiped, 'req was unpiped');
+  assert.equal(req._readableState.flowing, false, 'req not flowing');
+  assert.equal(req._readableState.pipesCount, 0, 'req has 0 pipes');
+});
+
+form.on('unpipe', function(){
+  unpiped = true;
+});
+
+form.parse(req)
+assert.equal(req._readableState.flowing, true, 'req flowing');
+assert.equal(req._readableState.pipesCount, 1, 'req has 1 pipe');
