17611a61eff26f28c423ca69c44f9d3861580e03
Douglas Christopher Wilson <doug@somethingdoug.com>
2015-05-08 22:40:38 -0400

    tests: add test for maxFieldsSize restriction

--- test/test.js
+++ test/test.js
@@ -466,7 +466,7 @@ var standaloneTests = [
     },
   },
   {
-    name: "issue 19",
+    name: "maxFields error",
     fn: function(cb) {
       var client;
       var server = http.createServer(function (req, res) {
@@ -503,6 +503,51 @@ var standaloneTests = [
           "------WebKitFormBoundaryvfUZhxgsZDO7FXLF\r\n" +
           "Content-Disposition: form-data; name=\"upload\"; filename=\"blah1.txt\"\r\n" +
           "Content-Type: text/plain\r\n" +
+          "\r\n" +
+          "hi1\r\n" +
+          "\r\n" +
+          "------WebKitFormBoundaryvfUZhxgsZDO7FXLF\r\n");
+      });
+    },
+  },
+  {
+    name: "maxFieldsSize error",
+    fn: function(cb) {
+      var client;
+      var server = http.createServer(function (req, res) {
+        var form = new multiparty.Form({maxFieldsSize: 8});
+        form.on('aborted', function () {
+          throw new Error("did not expect aborted");
+        });
+        var first = true;
+        form.on('error', function (err) {
+          assert.ok(first);
+          first = false;
+          client.end();
+          assert.ok(/maxFieldsSize/.test(err.message));
+          assert.equal(err.status, 413);
+          server.close(cb);
+        });
+        form.on('end', function () {
+          throw new Error('Unexpected "end" event');
+        });
+        form.on('field', function () {});
+        form.parse(req);
+      });
+      server.listen(function() {
+        client = net.connect(server.address().port);
+
+        client.write("POST /upload HTTP/1.1\r\n" +
+          "Content-Length: 678\r\n" +
+          "Content-Type: multipart/form-data; boundary=----WebKitFormBoundaryvfUZhxgsZDO7FXLF\r\n" +
+          "\r\n" +
+          "------WebKitFormBoundaryvfUZhxgsZDO7FXLF\r\n" +
+          "Content-Disposition: form-data; name=\"title\"\r\n" +
+          "\r\n" +
+          "foofoo" +
+          "\r\n" +
+          "------WebKitFormBoundaryvfUZhxgsZDO7FXLF\r\n" +
+          "Content-Disposition: form-data; name=\"text\"\r\n" +
           "\r\n" +
           "hi1\r\n" +
           "\r\n" +
