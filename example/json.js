7eeb7d91cf91824625d014ce06cfa93aab828ee6
Walle Cyril <cyril.walle@protonmail.com>
2019-12-04 23:57:39 +0100

    start with streams

--- example/json.js
+++ example/json.js
@@ -18,8 +18,7 @@ server = http.createServer(function(req, res) {
   form
     .on('error', function(err) {
       res.writeHead(500, {'content-type': 'text/plain'});
-      res.end('error:\n\n'+util.inspect(err));
-      console.error(err);
+      res.end('error:\n\n' + util.inspect(err));
     })
     .on('field', function(field, value) {
       console.log(field, value);
