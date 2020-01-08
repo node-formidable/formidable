2d2b630e8647cabc2be8ace5da4ee0235af014c0
Kedar <kedarv@users.noreply.github.com>
2020-01-08 02:57:37 -0800

    fix: remove gently hijack and tests (#539)
    fixes #337

--- test-legacy/common.js
+++ test-legacy/common.js
@@ -1,15 +1,7 @@
 var path = require('path'),
     fs = require('fs');
 
-try {
-  global.Gently = require('gently');
-} catch (e) {
-  throw new Error('this test suite requires node-gently');
-}
-
 exports.lib = path.join(__dirname, '../../lib');
-
-global.GENTLY = new Gently();
 
 global.assert = require('assert');
 global.TEST_PORT = 13532;
