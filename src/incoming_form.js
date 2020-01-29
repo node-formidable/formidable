c1dcb9c7bd8b15278ea679c305aa93ca29884e09
Charlike Mike Reagent <opensource@tunnckocore.com>
2020-01-29 02:30:50 +0200

    chore: note and ordering
    Signed-off-by: Charlike Mike Reagent <opensource@tunnckocore.com>

--- src/incoming_form.js
+++ src/incoming_form.js
@@ -207,11 +207,12 @@ class IncomingForm extends EventEmitter {
 
     // This MUST check exactly for undefined. You can not change it to !part.filename.
 
-    // @tunnckocore: no it can be any falsey value, it most probably depends on what's returned
+    // ? NOTE(@tunnckocore): no it can be any falsey value, it most probably depends on what's returned
     // from somewhere else. Where recently I changed the return statements
     // and such thing because code style
-    // @tunnckocore: or even better, if there is no mime, then it's for sure a field
-    if (!part.mime || (part.filename !== '' && !part.filename)) {
+    // ? NOTE(@tunnckocore): or even better, if there is no mime, then it's for sure a field
+    // ? NOTE(@tunnckocore): filename is an empty string when a field
+    if ((part.filename !== '' && !part.filename) || !part.mime) {
       let value = '';
       const decoder = new StringDecoder(part.transferEncoding || this.encoding);
 
