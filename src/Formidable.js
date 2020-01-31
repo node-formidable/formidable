a1028ecfdd725f2881baf86231fe8db01fab00ea
Dmitry Ivonin <dmitr-ivon@ya.ru>
2020-01-31 11:27:20 +0300

    fix(typo): rename 'maxFileSize' option (#566)
    * fix(typo): rename 'maxFileSize' option
    
    Co-authored-by: Charlike Mike Reagent <opensource@tunnckocore.com>

--- src/Formidable.js
+++ src/Formidable.js
@@ -336,7 +336,9 @@ class IncomingForm extends EventEmitter {
 
     this._error(
       new Error(
-        `bad content-type header, unknown content-type: ${this.headers['content-type']}`,
+        `bad content-type header, unknown content-type: ${
+          this.headers['content-type']
+        }`,
       ),
     );
   }
