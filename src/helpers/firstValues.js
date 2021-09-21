904d59e6c1b30508d592010a4ac90346dcfbc0ec
Cyril Walle <cyril.walle@protonmail.com>
2021-09-21 17:37:47 +0200

    feat: firstValues takes the form and does not operate on json forms

--- src/helpers/firstValues.js
+++ src/helpers/firstValues.js
@@ -1,8 +1,14 @@
+import { multipartType } from "../plugins/multipart.js";
+import { querystringType } from "../plugins/querystring.js";
+
 export { firstValues };
 
 /* Gets first values of fields, like pre 3.0.0 without multiples    
  pass in a list of exceptions where arrays of strings is still wanted */
-const firstValues = (fields, exceptions = []) => {
+const firstValues = (form, fields, exceptions = []) => {
+    if (form.type !== querystringType && form.type !== multipartType) {
+        return fields;
+    }
     return Object.fromEntries(Object.entries(fields).map(([key, value]) => {
         if (exceptions.includes(key)) {
             return [key, value];
