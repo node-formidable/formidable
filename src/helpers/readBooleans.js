9f2016675e8945c45b1580466e7800b86f40694f
Cyril Walle <cyril.walle@protonmail.com>
2021-09-21 17:39:12 +0200

    feat: make readBooleans compatible with json

--- src/helpers/readBooleans.js
+++ src/helpers/readBooleans.js
@@ -6,7 +6,7 @@ const readBooleans = (fields, listOfBooleans) => {
     // html forms do not send off at all
     const fieldsWithBooleans = Object.assign({}, fields);
     listOfBooleans.forEach(key => {
-        fieldsWithBooleans[key] = fields[key] === `on`;
+        fieldsWithBooleans[key] = fields[key] === `on` || fields[key] === true;
     });
     return fieldsWithBooleans;
 };
