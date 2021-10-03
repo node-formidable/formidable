3d7af8020b379141502f4f72269d106704cd9c9b
GrosSacASacs <cyril.walle@protonmail.com>
2021-10-03 07:54:32 +0200

    tests: fix some of the tests  (#763)
    * chore: up supertest jest and nyc
    
    * tests: convert to import , fix some tests
    
    * test: make malformed boundary as per comment ?
    
    * tests: refactor

--- test/fixture/js/preamble.js
+++ test/fixture/js/preamble.js
@@ -8,7 +8,7 @@ const crlf_http = [
     type: 'file',
     name: 'upload',
     originalFilename: 'plain.txt',
-    fixture: 'plain.txt',
+    fixture: 'crlf',
     sha1: 'b31d07bac24ac32734de88b3687dddb10e976872',
   },
 ];
@@ -18,7 +18,7 @@ const preamble_http = [
     type: 'file',
     name: 'upload',
     originalFilename: 'plain.txt',
-    fixture: 'plain.txt',
+    fixture: 'preamble',
     sha1: 'a47f7a8a7959f36c3f151ba8b0bd28f2d6b606e2',
   },
 ];
