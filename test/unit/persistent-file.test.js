8355c64ec4c5e3514f34aef87fb542decbd9bf29
Pedro S. Lopez <pedroslopez@me.com>
2021-12-06 17:43:30 -0400

    fix: PersistentFile.toString() doesn't throw (#796)
    * fix: PersistentFile.toString() doesn't throw
    
    * add test

--- test/unit/persistent-file.test.js
+++ test/unit/persistent-file.test.js
@@ -9,6 +9,7 @@ const file = new PersistentFile({
   type: 'image/png',
   lastModifiedDate: now,
   originalFilename: 'cat.png',
+  newFilename: 'dff1d2eaab9752165764dcd00',
   mimetype: 'image/png',
 });
 
@@ -29,6 +30,11 @@ describe('PersistentFile', () => {
     expect(obj.filepath).toBe('/tmp/cat.png');
     expect(obj.mimetype).toBe('image/png');
     expect(obj.originalFilename).toBe('cat.png');
+  });
+
+  test('toString()', () => {
+    const result = file.toString();
+    expect(result).toBe('PersistentFile: dff1d2eaab9752165764dcd00, Original: cat.png, Path: /tmp/cat.png')
   });
 
   test('destroy()', () => {
