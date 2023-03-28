d1c63d560a20ad03ed046f13eed0f427eaa091cb
Cyril Walle <cyril.walle@protonmail.com>
2023-03-28 20:58:11 +0200

    test: force carriage return and use fetch

--- test/standalone/end-event-emitted-twice.test.js
+++ test/standalone/end-event-emitted-twice.test.js
@@ -1,57 +1,63 @@
 import {strictEqual} from 'node:assert';
-import { createServer } from 'node:http';
-import { connect } from 'node:net';
+import { createServer, request } from 'node:http';
 import formidable from '../../src/index.js';
+import test from 'node:test'
 
 const PORT = 13539;
 
-test('end event emitted twice', (done) => {
-  const server = createServer((req) => {
+test('end event emitted twice', (t,done) => {
+  const server = createServer((req, res) => {
     const form = formidable();
 
     let i = 0;
     form.on('end', () => {
       i += 1;
-      strictEqual(i, 1, 'end should be emitted once');
+      strictEqual(i, 1, 'end should be emitted once  (on end)');
     });
     form.parse(req, () => {
-
-      server.close();
-      strictEqual(i, 1, 'end should be emitted once');
-      done();
+      try {
+        strictEqual(i, 1, 'end should be emitted once (callback)');
+      } catch (e) {
+        done();
+      }
+      res.writeHead(200);
+      res.end("ok")
     });
   });
 
-  server.listen(PORT, 'localhost', () => {
+  server.listen(PORT, () => {
     const chosenPort = server.address().port;
-
-    const client = connect(chosenPort);
-
-    client.write(
-`POST /api/upload HTTP/1.1
-Host: localhost:${chosenPort}
-User-Agent: N
-Content-Type: multipart/form-data; boundary=---------------------------13068458571765726332503797717
-
-
------------------------------13068458571765726332503797717
-Content-Disposition: form-data; name="title"
-
-a
------------------------------13068458571765726332503797717
-Content-Disposition: form-data; name="multipleFiles"; filename="x.txt"
-Content-Type: application/x-javascript
-
-
-
-a
-b
-c
-d
-
------------------------------13068458571765726332503797717--
-`,
-    );
-    client.end();
+    const  body = `----13068458571765726332503797717\r
+Content-Disposition: form-data; name="title"\r
+\r
+a\r
+----13068458571765726332503797717\r
+Content-Disposition: form-data; name="multipleFiles"; filename="x.txt"\r
+Content-Type: application/x-javascript\r
+\r
+\r
+\r
+a\r
+b\r
+c\r
+d\r
+\r
+----13068458571765726332503797717--\r
+`;
+    fetch(String(new URL(`http:localhost:${chosenPort}/`)), {
+      method: 'POST',
+      
+      headers: {
+        'Content-Length': body.length,
+        Host: `localhost:${chosenPort}`,
+        'Content-Type': 'multipart/form-data; boundary=--13068458571765726332503797717',
+      },
+      body
+    }).then(res => {
+      strictEqual(res.status, 200);
+        server.close();
+        done();
+    });
+   
   });
 });
