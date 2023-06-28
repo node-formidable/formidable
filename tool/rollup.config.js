4de86ae84af9082eda7a25fd1ee3781856fae25a
Cyril Walle <cyril.walle@protonmail.com>
2023-06-28 22:45:56 +0200

    feat: export more,
    make it work without having to write const formidable = require('formidable/index.cjs');

--- tool/rollup.config.js
+++ tool/rollup.config.js
@@ -5,6 +5,10 @@ import packageJson from '../package.json' assert {type: "json"};;
 
 const {dependencies} = packageJson;
 const plugins = [nodeResolve(), cjs()];
+const cjsOptions = {
+  format: `cjs`,
+  exports: `named`,
+}
 
 const external = [...Object.keys(dependencies)];
 
@@ -14,8 +18,84 @@ export default [
     output: [
       {
         file: `dist/index.cjs`,
-        format: `cjs`,
-        exports: `named`,
+        ...cjsOptions,
+      },
+    ],
+    external,
+    plugins,
+  },
+  {
+    input: `src/helpers/firstValues.js`,
+    output: [
+      {
+        file: `dist/helpers/firstValues.cjs`,
+        ...cjsOptions,
+      },
+    ],
+    external,
+    plugins,
+  },
+  {
+    input: `src/helpers/readBooleans.js`,
+    output: [
+      {
+        file: `dist/helpers/readBooleans.cjs`,
+        ...cjsOptions,
+      },
+    ],
+    external,
+    plugins,
+  },
+  {
+    input: `src/parsers/JSON.js`,
+    output: [
+      {
+        file: `dist/parsers/JSON.cjs`,
+        ...cjsOptions,
+      },
+    ],
+    external,
+    plugins,
+  },
+  {
+    input: `src/parsers/Multipart.js`,
+    output: [
+      {
+        file: `dist/parsers/Multipart.cjs`,
+        ...cjsOptions,
+      },
+    ],
+    external,
+    plugins,
+  },
+  {
+    input: `src/parsers/Querystring.js`,
+    output: [
+      {
+        file: `dist/parsers/Querystring.cjs`,
+        ...cjsOptions,
+      },
+    ],
+    external,
+    plugins,
+  },
+  {
+    input: `src/parsers/OctetStream.js`,
+    output: [
+      {
+        file: `dist/parsers/OctetStream.cjs`,
+        ...cjsOptions,
+      },
+    ],
+    external,
+    plugins,
+  },
+  {
+    input: `src/parsers/StreamingQuerystring.js`,
+    output: [
+      {
+        file: `dist/parsers/StreamingQuerystring.cjs`,
+        ...cjsOptions,
       },
     ],
     external,
